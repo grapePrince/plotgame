da0037821edefe19f533686ca4cf194bef95b4f5
Lee Yeji <tul90@Lee-ui-iMac.local>
2015-07-26 22:57:55 +0900

    mysqled2

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,7 +2,7 @@ var mDao = require('./dao');
  
 exports.getCardList = function(oReq, oRes, oQuery) {
 	if (oQuery.version) {
-		mDao.getCardList(oReq, oRes, Number(oQuery.version);
+		mDao.getCardList(oReq, oRes, Number(oQuery.version));
 	} else {
 		console.log('getCardList : invalid version')
 	}
